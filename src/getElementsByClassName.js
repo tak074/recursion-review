72694b9ced34f459304e58f84e06c45a58fc47ba
unknown <tak@ucsd.com>
2020-08-17 16:24:58 -0700

    $

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, element) {
+  //create node array
+  var nodes = [];
+
+  var element = element || document.body;
+  //checking if className is in current node
+  if (element.classList.contains(className)) {
+    //if yes, push it into node array.
+    nodes.push(element);
+  }
+
+  console.log(element.children);
+
+  for (var i = 0; i < element.children.length; i++) {
+    var miniNodes = getElementsByClassName(className, element.children[i]);
+    nodes = nodes.concat(miniNodes);
+  }
+
+  //return node array
+  return nodes;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null) {
+    return '' + obj;
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    var arrStr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arrStr.push(stringifyJSON(obj[i]));
+    }
+
+    return arrStr = '[' + arrStr + ']';
+  }
+
+  //object
+  if (typeof obj === 'object') {
+    var emp = [];
+    for (var key in obj) {
+      var objStr = stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+      emp.push(objStr);
+      if (obj[key] === undefined) {
+        return '{}';
+      }
+      //
+    }
+    return emp = '{' + emp + '}';
+  }
+
 };
